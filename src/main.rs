97e70337a75cf700be06689b414a168ae465a053
JFKongphop <kongphopleo@gmail.com>
2025-02-06 22:58:30 +0700

    refactor month filter and create null filter

--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,27 @@
-use polars::prelude::*;
-use serde::Deserialize;
-use futures::future;
-use tokio::sync::Mutex;
-use dotenv::dotenv;
-use std::{
-  env, 
-  error::Error, 
-  io::Cursor,
-  ops::BitAnd
+use std::error::Error;
+use running_rust::utils::{
+  fetch_data::fetch_text_csv, 
+  filter_column::{
+    month_filter,
+    null_filter
+  }
 };
-use polars::prelude::CsvReader;
-
-#[derive(Deserialize, Debug)]
-struct FileInfo {
-  download_url: String,
-}
+use dotenv::dotenv;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
   dotenv().ok();
-  let folder = env::var("FOLDER").unwrap();
-  let github_repo = format!("https://api.github.com/repos/{}", folder);
 
-  let resp = reqwest::Client::new()
-    .get(&github_repo)
-    .header("User-Agent", "github")
-    .send()
-    .await?
-    .json::<Vec<FileInfo>>()
-    .await?;
+  let running_df = fetch_text_csv().await?;
+  let running_df = null_filter(&running_df)?
+    .sort(["Date"], Default::default())?;
 
-  let csv_links: Vec<String> = resp
-    .into_iter()
-    .map(|file: FileInfo| file.download_url)
-    .collect();
+  let jan_2025 = month_filter(&running_df, "2568-01")?;
 
-  let csv_data: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
-  let header = String::from("Date,Energy (kcal),Activity,Distance(km),Duration(min),Pace(min),Heart rate: Average(min),Heart rate: Maximum(min)\n");
+  
 
-  let tasks: Vec<_> = csv_links.into_iter().map(|link| {
-    let csv_data = Arc::clone(&csv_data);
-    let header = header.clone();
 
-    tokio::spawn(async move {
-      match reqwest::get(&link).await {
-        Ok(response) => {
-          match response.text().await {
-            Ok(text) => {
-              let mut csv_data = csv_data.lock().await;
-              let data_bytes = text.as_bytes();
-              let cursor = Cursor::new(data_bytes);
-
-              match CsvReader::new(cursor).finish() {
-                Ok(_df) => {
-                  csv_data.push(text.replace(header.as_str(), ""));
-                }
-                Err(e) => eprintln!("Error reading CSV from {}: {}", link, e),
-              }
-            }
-            Err(e) => eprintln!("Error fetching CSV from {}: {}", link, e),
-          }
-        }
-        Err(e) => eprintln!("Error downloading from {}: {}", link, e),
-      }
-    })
-  }).collect();
-
-  future::join_all(tasks).await;
-
-  let joined_csv_data = {
-    let csv_data = csv_data.lock().await;
-    csv_data.join("\n")
-  };
-
-  let new_data = format!("{}{}", header, joined_csv_data);
-  let data_bytes = new_data.as_bytes();
-  let cursor = Cursor::new(data_bytes);
-
-  let mut running_df: DataFrame = CsvReader::new(cursor)
-    .finish()
-    .expect("CSV reading should not fail");
+  println!("{:?}", jan_2025);
 
   Ok(())
 }
--- src/utils/filter_column.rs
+++ src/utils/filter_column.rs
@@ -1,16 +1,16 @@
 use polars::prelude::*;
 use std::ops::BitAnd;
 
-use super::times::convert_date_timestamp;
+type PolarsFrame = PolarsResult<DataFrame>;
 
-pub fn activity_filter(df: &DataFrame, activity: &str) -> PolarsResult<DataFrame> {
+pub fn activity_filter(df: &DataFrame, activity: &str) -> PolarsFrame {
   let activity_column = df.column("Activity")?.str()?;
   let mask = activity_column.equal(activity);
   
   df.filter(&mask)
 }
 
-pub fn year_filter(df: &DataFrame, year: &str) -> PolarsResult<DataFrame> {
+pub fn year_filter(df: &DataFrame, year: &str) -> PolarsFrame {
   let date_column = df.column("Date")?.str()?;
   let mask = date_column
     .into_iter()    
@@ -20,7 +20,17 @@ pub fn year_filter(df: &DataFrame, year: &str) -> PolarsResult<DataFrame> {
   df.filter(&mask)
 }
 
-pub fn distance_filter(df: &DataFrame, min: f64, max: f64) -> PolarsResult<DataFrame> {
+pub fn month_filter(df: &DataFrame, year_month: &str) -> PolarsFrame {
+  let date_column = df.column("Date")?.str()?;
+  let mask = date_column
+    .into_iter()
+    .map(|date| date.and_then(|d| Some(d.starts_with(year_month))))
+    .collect::<BooleanChunked>();
+
+  df.filter(&mask)
+}
+
+pub fn distance_filter(df: &DataFrame, min: f64, max: f64) -> PolarsFrame {
   let distance_column = df.column("Distance(km)")?.f64()?;
   let mask = distance_column
     .gt(min)
@@ -29,29 +39,6 @@ pub fn distance_filter(df: &DataFrame, min: f64, max: f64) -> PolarsResult<DataF
   df.filter(&mask)
 }
 
-pub fn month_filter(df: &DataFrame, year_month: &str) -> PolarsResult<DataFrame> {
-  let date_part: Vec<i64> = year_month
-    .split('-')
-    .filter_map(|part| part.parse::<i64>().ok()) 
-    .collect();
-  let (year, month) = (date_part[0], date_part[1]);
-
-  let end_month = if month == 12 {
-    format!("{}-{}", year + 1, 1)
-  } else {
-    format!("{}-{}", year, month + 1)
-  };
-
-  let start_date = format!("{}-01 00:00:00", year_month);
-  let end_date = format!("{}-01 00:00:00", end_month);
-
-  let start_timestamp = convert_date_timestamp(&start_date);
-  let end_timestamp = convert_date_timestamp(&end_date);
-
-  let distance_column = df.column("Timestamp")?.i64()?;
-  let mask = distance_column
-    .gt(start_timestamp)
-    .bitand(distance_column.lt(end_timestamp));
-
-  df.filter(&mask)
+pub fn null_filter(df: &DataFrame) -> PolarsFrame {
+  df.filter(&df.column("Date")?.is_not_null())
 }
