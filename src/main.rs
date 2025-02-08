f5c193ad37717e6921299c51319eeaebfa4550fd
JFKongphop <kongphopleo@gmail.com>
2025-02-08 23:47:26 +0700

    use filter distance

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::error::Error;
 use running_rust::utils::{
+  apply_column::activity_to_type, 
   fetch_data::fetch_text_csv, 
   filter_column::{
-    month_filter,
-    null_filter
+    activity_filter, distance_filter, null_filter
   }
 };
 use dotenv::dotenv;
@@ -13,15 +13,22 @@ async fn main() -> Result<(), Box<dyn Error>> {
   dotenv().ok();
 
   let running_df = fetch_text_csv().await?;
-  let running_df = null_filter(&running_df)?
-    .sort(["Date"], Default::default())?;
+  let mut running_df = null_filter(&running_df)?.sort(["Date"], Default::default())?;
+  let running_df = running_df.apply("Activity", activity_to_type)?;
 
-  let jan_2025 = month_filter(&running_df, "2568-01")?;
+  let _indoor = activity_filter(&running_df, "indoor")?;
+  let _outdoor = activity_filter(&running_df, "outdoor")?;
 
-  
+  let _400m = distance_filter(&running_df, 0.39, 0.43)?;
+  let _1000m = distance_filter(&running_df, 1.0, 1.1)?;
+  let _1200m = distance_filter(&running_df, 1.2, 1.35)?;
+  let _2000m = distance_filter(&running_df, 2.0, 2.25)?;
 
 
-  println!("{:?}", jan_2025);
+  println!("{:?}", _400m);
+  println!("{:?}", _1000m);
+  println!("{:?}", _1200m);
+  println!("{:?}", _2000m);
 
   Ok(())
 }
